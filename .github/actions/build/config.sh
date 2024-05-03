825b128baa48b7c1ba646cbbf5d69bbf4eed4162
Michael Legleux <mlegleux@ripple.com>
2024-05-03 10:54:39 -0700

    Use rippled remote

--- .github/actions/build/config.sh
+++ .github/actions/build/config.sh
@@ -4,5 +4,5 @@ conan profile new --detect default
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan profile update settings.compiler.cppstd=20 default
 
-conan remote add --insert 0 redirectory https://conan.jfreeman.dev
-# conan remote add --insert 1 conan-non-prod http://18.143.149.228:8081/artifactory/api/conan/conan-non-prod
+conan remote add --insert 0 ripple http://18.143.149.228:8081/artifactory/api/conan/conan-non-prod
+conan remote add --insert 1 redirectory https://conan.jfreeman.dev
