1476b629fad5425a7b3f8c245a93360774a9d54d
Michael Legleux <mlegleux@ripple.com>
2024-05-08 10:52:31 -0700

    Run it all
    x-heavy

--- .github/actions/build/build_xbwd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source_dir=$PWD
-build_dir=$PWD/build
-cd .github/actions/build
-. ./deps.sh
-. ./config.sh
-. ./build.sh
--- .github/actions/build/deps.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update && apt-get install --yes python3-pip python-is-python3
-
-pip install "conan<2"
-pip install cmake
